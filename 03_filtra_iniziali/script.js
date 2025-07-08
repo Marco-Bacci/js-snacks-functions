4aceabce091bcea333745c4b62753bef058a48cd
Marco Bacci <marcobacci94@gmail.com>
2025-07-08 16:33:19 +0200

    feat: exercise 3 set up

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function lettersAndWords (array , letter){
+  // dichiaro la variabile dell'array contenente parole con stessa lettera e la variabile della lettera
+  const finalArray=[]
+  for (let i = 0; i<array.length; i++){
+    if(array[i].charAt(0) === `letter`){
+      finalArray.push(array[i])
+    }
 
+  }
+
+  return finalArray
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(lettersAndWords(names, L))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
