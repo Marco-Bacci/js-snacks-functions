bf6993076507765c378ba0c24ac7f0d83d1f81ec
Marco Bacci <marcobacci94@gmail.com>
2025-07-08 17:03:36 +0200

    fix:  letter in upper case

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,23 +2,24 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+// const letter= prompt(`Inserirsci una lettera: `)
 
 // Dichiara la funzione qui.
 function lettersAndWords (array , letter){
   // dichiaro la variabile dell'array contenente parole con stessa lettera e la variabile della lettera
   const finalArray=[]
+  
   for (let i = 0; i<array.length; i++){
-    if(array[i].charAt(0) === `letter`){
+    if(array[i].charAt(0) === letter.toUpperCase()){
       finalArray.push(array[i])
     }
-
+    
   }
-
+  
   return finalArray
 }
-
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(lettersAndWords(names, L))
+console.log(lettersAndWords(names, `a` ))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
