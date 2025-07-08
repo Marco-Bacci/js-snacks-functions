96a79a14ba506576c8718120172a2f3093a4aefb
Marco Bacci <marcobacci94@gmail.com>
2025-07-08 18:44:43 +0200

    feat: split string

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,19 @@
 
 const word = 'javascript';
 
+const newArray = []
 
 // Dichiara la funzione qui.
-
+function giveVocals (string){
+  splitString = string.split("")
+  for(let i=0; i<splitString.length; i++ ){
+    console.log(splitString[i])
+  }
+  
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+giveVocals(word)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
