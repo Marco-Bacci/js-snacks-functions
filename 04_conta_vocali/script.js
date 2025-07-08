cd48373a8d678d13a006a1d398d3f5e6eb31379e
Marco Bacci <marcobacci94@gmail.com>
2025-07-08 19:04:08 +0200

    fix: order exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -8,23 +8,23 @@ function giveVocals (string){
   const foundVocals = []
   // splitto la stringa in un array di lettere
   splitString = string.split("")
-  const vocals= [`a`,`e`,`i`, `o`, `u`]
-  // ciclo le letetre dell'array creato per metterle a paragone con array delle vocali
+  const vocals= [`a`,`e`,`i`, `o`, `u`] //array delle vocali
+  // doppio ciclo for per comparare le lettere dell'array creato con quello delle vocali
   for(let i=0; i<splitString.length; i++ ){
-// creo un altro ciclo per comparare ogni lettera della array con un nuvoo array delle vocali
     for (let j = 0; j<vocals.length; j++){
       if(splitString[i]=== vocals[j]){
         foundVocals.push(vocals[j])
-        
       }
     }
-
   }
-  return foundVocals
-}
+  
+    return foundVocals
+  }
+        
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
 console.log(giveVocals(word))
+
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
