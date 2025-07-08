1187fb45f7c80bf99f38f2f3138eecf72428c429
Marco Bacci <marcobacci94@gmail.com>
2025-07-08 19:02:31 +0200

    feat : exercise finalised

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,20 +1,30 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = 'supercalifragilistichespiralidoso';
 
-const newArray = []
 
 // Dichiara la funzione qui.
 function giveVocals (string){
+  const foundVocals = []
+  // splitto la stringa in un array di lettere
   splitString = string.split("")
+  const vocals= [`a`,`e`,`i`, `o`, `u`]
+  // ciclo le letetre dell'array creato per metterle a paragone con array delle vocali
   for(let i=0; i<splitString.length; i++ ){
-    console.log(splitString[i])
+// creo un altro ciclo per comparare ogni lettera della array con un nuvoo array delle vocali
+    for (let j = 0; j<vocals.length; j++){
+      if(splitString[i]=== vocals[j]){
+        foundVocals.push(vocals[j])
+        
+      }
+    }
+
   }
-  
+  return foundVocals
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-giveVocals(word)
 
+console.log(giveVocals(word))
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
